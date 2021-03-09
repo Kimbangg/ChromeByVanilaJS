94d2a23a577b24f7c5eb6b12a850674fb6b664f0
Donghyeon <Kimbangg@MacBook-Pro.local>
2021-03-09 23:04:42 +0900

    # Making & Displaying ToDo list

---
+++ todo.js
@@ -0,0 +1,55 @@
+const toDoform = document.querySelector('.js-toDoForm'),
+    toDoInput = toDoform.querySelector('input');
+    toDoList = document.querySelector('.js-toDoList');
+
+const TODOS_LS = 'toDos';
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+    //localStorage에는 js 데이터를 저장 할 수 없다.
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn =document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerHTML = "❌";
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text,
+        id: newId,
+    }
+    toDos.push(toDoObj);
+    saveToDos();
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+      const parsedToDos = JSON.parse(loadedToDos);
+      parsedToDos.forEach(function(toDo) {
+        paintToDo(toDo.text);
+      });
+    }
+  }
+  
+
+function init () {
+    loadToDos();
+    toDoform.addEventListener("submit", handleSubmit);
+}
+
+init();
