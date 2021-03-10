8d1026a064197710024f70e7339841ebd52f3d61
kimbangg <ehehdgus1@hanyang.ac.kr>
2021-03-10 17:14:32 +0900

    # UI 및 복습 완료

--- clock.js
+++ clock.js
@@ -1,4 +1,5 @@
 const clockContainer = document.querySelector('.js-clock');
+// js-clock 이라는 div 안에 있는 h1에 대해서 영향을 주기 위해서 변수로 설정
 const clockTitle = clockContainer.querySelector('h1');
 
 function getTime() {
@@ -6,6 +7,7 @@ function getTime() {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
+    // 조건문을 사용해서 10 이라는 숫자 아래로 가면 0을 붙여서 시계와 비슷한 형태로 조정
     clockTitle.innerText = `${
         hours < 10 ? `0${hours}` : hours}:${
         minutes < 10 ? `0${minutes}` : minutes}:${
--- gretting.js
+++ gretting.js
@@ -1,4 +1,6 @@
+// js-form 에 js를 주기 위해서 form 이라는 변수에 담는다.
 const form = document.querySelector(".js-form"),
+// form 안에 있는 input만을 가져옴
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greetings");
 
@@ -22,16 +24,21 @@ function askForName() {
 }
 
 function paintGreeting(text) {
+  // 이전에 적혀있던 내용이 있다면 그 것을 지우고
   form.classList.remove(SHOWING_CN);
+  // Class 뒤에 Showing을 추가 시켜 줌으로써, none -> block으로 전환
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
+  // localStorage를 통해 값을 얻어 올 수 없다면
   if (currentUser === null) {
     askForName();
+    // localStorage를 통해 값을 얻어 올 수 있다면
   } else {
+    // 저장된 이름을 통해서 Hello + 이름을 출력한다.
     paintGreeting(currentUser);
   }
 }
